87f2e95c25d4b6a4b0a0c16c936e45d3bfd461c7
Jayesh Tembhekar <jtembhekar2@gmail.com>
2020-05-17 23:24:48 +0530

    New concepts

---
+++ src/Strings.kt
@@ -0,0 +1,30 @@
+var message = "Hello Fish"
+
+val msg1 = "Hello"
+val msg2 = "fish"
+
+
+
+var number1 = 12
+var number2 = 8
+
+//"I have ${number1 + number2} fish and plants"
+
+
+val fish = "fish"
+val plant = "plant"
+
+//fish === plant        returns False
+
+//fish !== plant        returns True
+
+
+val numberOfFish = 50
+val numberOfPlants = 23
+
+//if(numberOfFish > numberOfPlants) println("Good ratio")
+//        else
+//        println("Unhealthy ratio")
+
+val fish = 50
+if(fish in 10..100) println(fish)
